b0a9cfdc0486a03993ddd70ac2c3bcc265cb5f59
“daffadev-inc” <daffadev.inc@gmail.com>
2022-09-08 11:45:15 +0700

    add modal react

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,10 +1,20 @@
+import { useAddress, useMetamask, useWalletConnect, useCoinbaseWallet, useDisconnect } from "@thirdweb-dev/react";
+import React, { useState } from "react";
 import Image from 'next/image'
 import { useIntl } from 'react-intl'
 import utilStyles from '../../styles/App.module.scss'
 import Link from 'next/link'
+import Modal from '../modal/Modal'
 
 export default function Banner({ ina }) {
+  const [showModal, setShowModal] = useState(false);
+
   const intl = useIntl();
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  const connectWithWalletConnect = useWalletConnect();
+  const connectWithCoinbaseWallet = useCoinbaseWallet();
+
     return (
     <div className={utilStyles.main}>
         <div className={utilStyles.main__container}>
@@ -14,7 +24,15 @@ export default function Banner({ ina }) {
             <div className={utilStyles.main__content}>
                 <h1> {intl.formatMessage({ id: 'join' })} <span className={utilStyles.textHighlight}>{intl.formatMessage({ id: 'movement' })}</span> {intl.formatMessage({ id: 'and' })} <span className={utilStyles.textHighlight}>{intl.formatMessage({ id: 'unlock' })}</span> {intl.formatMessage({ id: 'unique' })} <span className={utilStyles.textHighlight}>{intl.formatMessage({ id: 'art' })}</span></h1>
                 <p>{intl.formatMessage({ id: 'discovered' })}</p>
+      {address ? (
                 <button className={utilStyles.main__btn}><a href="#">{intl.formatMessage({ id: 'view' })}</a></button>
+      ) : (
+<>
+                <button className={utilStyles.main__btn}><a onClick={() => setShowModal(true)}>{intl.formatMessage({ id: 'withSuffix' })}</a></button>
+            <Modal onClose={() => setShowModal(false)}
+                show={showModal}></Modal>
+</>
+        )}
             </div>
         </div>
     </div>
